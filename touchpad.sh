3ad391d6676c247609f742379ef781f372bb4698
Mikhail Klementyev <jollheef@riseup.net>
2012-05-26 12:45:20 +0400

    Add touchpad.sh to .xinitrc and remove sleep in touchpad.sh

--- touchpad.sh
+++ touchpad.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 8
 xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 8 1
 xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Pressure" 32 10
 xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Width" 32 9
