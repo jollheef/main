8586d59bf16d0e1f840026921e8552d710df8cfd
Mikhail Klementyev <jollheef@riseup.net>
2012-06-17 14:13:31 +0400

    update hotkey

--- init.el
+++ init.el
@@ -43,6 +43,8 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x C-m") 'execute-extended-command)
 (global-set-key (kbd "C-c C-m") 'execute-extended-command)
+(global-set-key (kbd "C-x C-k") 'kill-region)
+(global-set-key (kbd "C-c C-k") 'kill-region)
 (global-set-key (kbd "<f12>")
 		(lambda () 
 		  (interactive) 
