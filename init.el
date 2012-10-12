cc039cb7c83fb847ba7c21c88e9605a6b3893cb9
Mikhail <michael@darkstar.example.net>
2012-10-12 22:16:07 +0400

    Sync

--- init.el
+++ init.el
@@ -166,6 +166,20 @@
                 (vector (append mod (list to)))))))))
     (when input-method
       (activate-input-method current))))
+;; Авто определение формата по расширению файла
+(add-to-list 'auto-mode-alist '(".fb2$" . fb2-mode-view))
+;; Функция для файлов .fb2 в режиме просмотра
+(defun fb2-mode-view()
+  (vc-toggle-read-only)
+  (interactive)
+  (sgml-mode)
+  (sgml-tags-invisible 0))
+;; Функция для файлов .fb2 в режиме редактирования
+(defun fb2-mode-edit()
+  (vc-toggle-read-only nil)
+  (interactive)
+  (sgml-mode)
+  (sgml-tags-invisible 0))
 
 ;;
 ;;
